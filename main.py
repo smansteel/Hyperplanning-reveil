1a7e25308865e8c154ac8a63385d06ce639db52c
smansteel <59889389+smansteel@users.noreply.github.com>
2020-10-03 12:57:40 +0200

    updates

--- main.py
+++ main.py
@@ -5,32 +5,37 @@ import pytz
 import os
 from dotenv import load_dotenv
 load_dotenv()
-tempsdeglande = 7200
-etatmail = 0
+tempsdeglande = 7200 #temps a reveiller avant le début des cours en secondes
+
 transfert = ""
 D = []
 bla = []
 datetrouvee = False
 cours_string = ""
-null = "null"
+ajd = datetime.date.today().strftime("%Y%m%d")
+demain_nf = datetime.date.today() + datetime.timedelta(days=1)
+demain = demain_nf.strftime("%Y%m%d")
+cours = []
+
 
 def take_second(elem):
     return elem[:7]
 
-url = os.getenv('url')
+url = os.getenv('url_calendrier')
 
 
 def download_calendar():
     r = requests.get(url, allow_redirects=True)
     open('cal.ics', 'wb').write(r.content)
+    print("cal updated")
 
-
-with open('cal.ics', 'r') as my_file:
+download_calendar()
+with open('cal.ics', 'r') as my_file: #hopla gros bourrin on met tout dans un string
     for ligne in my_file:
         transfert += ligne
-download_calendar
+
 bla = transfert.split("BEGIN:VEVENT")
-cours = []
+
 for i in range(len(bla)):
     cours += tuple(bla[i].split("\n"))
 
@@ -40,11 +45,9 @@ for i in range(1, len(bla) - 1):
 datesdecours = cours_string.split(" DTSTART:")
 datesdecours[0] = datesdecours[0][8:]
 datesdecours.sort()
-ajd = datetime.date.today().strftime("%Y%m%d")
-demain_nf = datetime.date.today() + datetime.timedelta(days=1)
-demain = demain_nf.strftime("%Y%m%d")
 
-for i in range(0, len(datesdecours)):
+
+for i in range(0, len(datesdecours)): #on cherche le prochain cours
     if datesdecours[i][:8] < demain:
         pass
     else:
@@ -52,31 +55,28 @@ for i in range(0, len(datesdecours)):
             print(datesdecours[i])
             datecoursdemain = datesdecours[i]
             datetrouvee = True
-
         else:
             break
 for i in range(0,len(bla)):
     if datecoursdemain in bla[i]:
         numerodecours = i
-print(numerodecours)
+#print(numerodecours)
+#print(infocours)
+#print(infocours[infocours.find("DESCRIPTION;"):infocours.find("\nEND")])#essai de traiter les données pour les mettre en libéllé de l'alarme
 
 infocours = str(bla[numerodecours])
-print(infocours)
-print(infocours[infocours.find("DESCRIPTION;"):infocours.find("\nEND")])
+if datecoursdemain == demain : # On vérifie qu'on a cours demain, ça serait ballot quand même
 
-
-if datecoursdemain == demain :
-
-    coursutc = datetime.datetime.strptime(datecoursdemain, '%Y%m%dT%H%M%SZ')
+    coursutc = datetime.datetime.strptime(datecoursdemain, '%Y%m%dT%H%M%SZ') #conversion de temps UTC à temps Local
     target_tz = pytz.timezone('Europe/Paris')
     local_tz = pytz.timezone('UTC')
     courshdp = local_tz.localize(coursutc)
     courshdp = target_tz.normalize(courshdp)
 
-    alarm = (courshdp.hour * 60 + courshdp.minute) * 60 + courshdp.second
-    gmailacc = os.getenv('gmailacc')
+    alarm = (courshdp.hour * 60 + courshdp.minute) * 60 + courshdp.second #conversion en secondes
+    gmailacc = os.getenv('gmailacc') #on obtient les variables
     gmailsecret = os.getenv('gmailsecret')
     print(alarm-tempsdeglande)
     datas=f"secret={gmailsecret}&to={gmailacc}&device=&priority=normal&payload={alarm-tempsdeglande}"
     print(datas)
-    print(requests.post('https://llamalab.com/automate/cloud/message',data=datas,headers={'content-type': 'application/x-www-form-urlencoded'}))
+    print(requests.post('https://llamalab.com/automate/cloud/message',data=datas,headers={'content-type': 'application/x-www-form-urlencoded'})) #la requête finale !
