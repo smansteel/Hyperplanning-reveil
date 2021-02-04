0c6720ef3a9aa7f89215cab30b6157516c3966d9
smansteel <59889389+smansteel@users.noreply.github.com>
2021-02-04 22:41:30 +0100

    smoll update pour eviter d'être reveiller 1 jour trop tot

--- main.py
+++ main.py
@@ -33,11 +33,9 @@ with open('cal.ics', 'r') as my_file: #hopla gros bourrin on met tout dans un st
         transfert += ligne
 
 temp = transfert.split("BEGIN:VEVENT")
-#print(temp)
 for i in range(0, len(temp)):
     bla += " "
 for i in range(0,len(temp)):
-    #print(temp[i])
     etatcours = temp[i][temp[i].find("UID:")+4:temp[i].find("TARDIEU")-8]
     if etatcours[1]== "o":
         bla[i] = temp[i]
@@ -47,7 +45,7 @@ for i in range(len(bla)):
     cours += tuple(bla[i].split("\n"))
 for i in range(0,28):
     bla.pop()
-#print(bla)
+
 for i in range(1, len(bla) - 1):
     cours_string += bla[i].split("\n")[5] + " " #C'est ici le parsing des infos
     lieustring += bla[i].split("\n")[8] + " "
@@ -58,17 +56,15 @@ datesdecours = [cours_string.split("DTSTART:"),lieustring.split("LOCATION;LANGUA
 """'for i in range(0,len(datesdecours)):
     if len(datesdecours[0][i]) == 8:
         removelist += datesdecours[0][i]'"""
-print(datesdecours)
+
 lieudecours = lieustring.split("LOCATION;LANGUAGE=fr:")
-print(len(lieudecours), len(datesdecours[0]))
-#print(datesdecours)
+
+
 datesdecours[0].sort()
 
 
 for i in range(0, len(datesdecours[0])): #on cherche le prochain cours
-    print(i,len(datesdecours),datesdecours[0][i][:8],demain)
-    if datetrouvee == False and datesdecours[0][i][:8] >= demain:
-        print(i)
+    if datetrouvee == False and datesdecours[0][i][:8] == demain:
         datecoursdemain = datesdecours[0][i]
         lieu_cours = datesdecours[0][i]
         datetrouvee = True
@@ -76,12 +72,10 @@ for i in range(0, len(datesdecours[0])): #on cherche le prochain cours
 for i in range(0,len(bla)):
     if datecoursdemain[:8] in temp[i].split("\n")[5]:
         numerodecours = i
-#print(numerodecours)
-#print(infocours)
-#print(infocours[infocours.find("DESCRIPTION;"):infocours.find("\nEND")])#essai de traiter les données pour les mettre en libéllé de l'alarme
-print(bla[numerodecours])
+
+
 infocours = str(bla[numerodecours])
-print(lieu_cours)
+
 
 coursutc = datetime.datetime.strptime(datecoursdemain, '%Y%m%dT%H%M%SZ ') #conversion de temps UTC à temps Local
 
@@ -105,3 +99,4 @@ with open("horaires.txt", "w") as horaires:
     horaires.write(str(wakeuptime))
     horaires.write("\n")
     horaires.write(str(msg))
+    print(wakeuptime, msg)
