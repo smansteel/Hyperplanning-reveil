e5eac54db73ea5ccf0620549a12a7420601653d3
smansteel <59889389+smansteel@users.noreply.github.com>
2021-02-04 22:24:57 +0100

    Update utilisateur0.py

--- utilisateur0.py
+++ utilisateur0.py
@@ -1,5 +1,3 @@
-gmailacc = "maxime.tardieu%40gmail.com" #Compte gmail lié avec un %40 a la place de l'@
-gmailsecret = "2.CMV8QKX56HcUo_WdqYD8BovkNoI0gNEcOX9hQIFfBe8" #secret lié au compte, peut être genérée a l'adresse suivante :
 url_calendrier ="https://planning-2021.isep.fr/Telechargements/ical/Edt_TARDIEU.ics?version=2018.0.3.1&idICal=EA705ABDB51740B2D372D527B5202649&param=643d5b312e2e36325d2666683d3126663d31"
 temps_ndc = 7200
 temps_ndl = 7200
